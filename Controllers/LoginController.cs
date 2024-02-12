59172d88bbc31b71e4f50a7c398b0ba9651e761c
LucianoCV01 <Lucianocosentino01@gmail.com>
2024-02-11 21:27:43 -0300

    WIP: ViewModel de Login Creados

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -2,6 +2,7 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using tl2_tp10_2023_LucianoCV01.Models;
 using tl2_tp10_2023_LucianoCV01.Repository;
+using tl2_tp10_2023_LucianoCV01.ViewModels;
 
 namespace tl2_tp10_2023_LucianoCV01.Controllers;
 
@@ -17,16 +18,18 @@ public class LoginController : Controller
     }
     public IActionResult Index()
     {
-        return View(new Usuario());
+        return View(new LoginViewModel());
     }
     public IActionResult Login(Usuario usuario)
     {
+        //existe el usuario?
         List<Usuario> usuarios = repositorioUsuario.GetAll();
         var usuarioLogin = usuarios.FirstOrDefault(u => u.NombreDeUsuario == usuario.NombreDeUsuario && u.Contrasenia == usuario.Contrasenia);
-
+        // si el usuario no existe devuelvo al index
         if (usuarioLogin == null) return RedirectToAction("Index");
-
+        //Registro el usuario
         logearUsuario(usuarioLogin);
+        //Devuelvo el usuario al Home
         return RedirectToRoute(new { controller = "Home", action = "Index" });
     }
 
